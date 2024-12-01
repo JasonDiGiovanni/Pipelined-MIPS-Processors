9d647898a33237214906bc22fd93458e1efeec82
JasonDiGiovanni <125398494+JasonDiGiovanni@users.noreply.github.com>
2024-12-01 13:43:11 -0600

    Delete cpre381-toolflow/proj/mips/Proj2_BRANCH_1.s

--- cpre381-toolflow/proj/mips/Proj2_BRANCH_1.s
+++
@@ -1,4 +0,0 @@
-# BRANCH_1: Test a taken branch with instructions after the branch flushed
-BEQ R1, R2, Label1   # Branch taken
-NOP                  # This instruction will be flushed
-Label1: ADD R3, R4, R5
